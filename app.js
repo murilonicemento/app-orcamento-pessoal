9fbedb15823658dc488ecea29100d7935f786436
murilonicemento <murilo_nascimento22@outlook.com>
2022-07-13 18:06:16 -0300

    Inclusão de alterações e aprimoramento do código

--- app.js
+++ app.js
@@ -34,11 +34,19 @@ class Bd {
         localStorage.setItem("id", id)
     }
     recuperarTodosRegistros() {
+        //array despesas
+        let despesas = Array()
         let id = localStorage.getItem("id")
-        for (let i = 1; i <= id; i++){
+        //recupera as despesas cadastradas em localStorage
+        for (let i = 1; i <= id; i++) {
             let despesa = JSON.parse(localStorage.getItem(i))
-            console.log(despesa)
+            //existe a possibilidade de haver índices pulados/removidos
+            if (despesa === null) {
+                continue
+            }
+            despesas.push(despesa)
         }
+        return despesas
     }
 }
 let bd = new Bd()
@@ -60,19 +68,22 @@ function registrarDespesas() {
         document.getElementById("modal_conteudo").innerHTML = "Despesa foi cadastrada com sucesso"
         document.getElementById("modal_btn").innerHTML = "Voltar"
         document.getElementById("modal_btn").className = "btn btn-success"
+        //dialog de sucesso
         $("#modalRegistraDespesas").modal("show")
+        //limpar formulário
+        ano.value = ""
+        mes.value = ""
+        dia.value = ""
+        tipo.value = ""
+        descricao.value = ""
+        valor.value = ""
     } else {
         document.getElementById("modal_titulo").innerHTML = "Erro na inclusão do registro"
         document.getElementById("modal_titulo_div").className = "modal-header text-danger"
         document.getElementById("modal_conteudo").innerHTML = "Erro na gravação, verifique se todos os campos foram preenchidos corretamente"
         document.getElementById("modal_btn").innerHTML = "Voltar e corrigir"
         document.getElementById("modal_btn").className = "btn btn-danger"
+        //dialog de erro
         $("#modalRegistraDespesas").modal("show")
     }
-
 }
-function carregaListaDespesas() {
-    bd.recuperarTodosRegistros()
-}
-
-carregaListaDespesas()
