0c9243abea45f91cd179c7a26015652e187b4b37
murilonicemento <murilo_nascimento22@outlook.com>
2022-07-14 11:35:06 -0300

    Inclusão de novas funcionalidades

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ class Despesas {
         return true
     }
 }
-
 class Bd {
     constructor() {
         let id = localStorage.getItem("id")
@@ -48,7 +47,37 @@ class Bd {
         }
         return despesas
     }
+    pesquisar(despesa) {
+        let despesasFiltradas = Array()
+        despesasFiltradas = this.recuperarTodosRegistros()
+        //ano
+        if (despesa.ano != "") {
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+        }
+        //mês
+        if (despesa.mes != "") {
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+        }
+        //dia
+        if (despesa.dia != "") {
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+        }
+        //tipo
+        if (despesa.tipo != "") {
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)
+        }
+        //descrição
+        if (despesa.descricao != "") {
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+        }
+        //valor
+        if (despesa.valor != "") {
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
+        }
+        return despesasFiltradas
+    }
 }
+
 let bd = new Bd()
 
 function registrarDespesas() {
@@ -86,4 +115,42 @@ function registrarDespesas() {
         //dialog de erro
         $("#modalRegistraDespesas").modal("show")
     }
+}
+
+function pesquisarDespesas() {
+    let ano = document.getElementById("ano").value
+    let mes = document.getElementById("mes").value
+    let dia = document.getElementById("dia").value
+    let tipo = document.getElementById("tipo").value
+    let descricao = document.getElementById("descricao").value
+    let valor = document.getElementById("valor").value
+
+    let despesas = new Despesas(ano, mes, dia, tipo, descricao, valor)
+    let despesa = bd.pesquisar(despesas)
+    let listaDespesas = document.getElementById("listaDespesas")
+    //percorrer o array despesas
+    //percorrer o array despesas
+    despesa.forEach(function (d) {
+        console.log(d)
+        //criando as linhas(tr)
+        let linha = listaDespesas.insertRow()
+        //criando as colunas(td)
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        //ajustar o tipo
+        switch (d.tipo) {
+            case "1": d.tipo = "Alimentação"
+                break
+            case "2": d.tipo = "Educação"
+                break
+            case "3": d.tipo = "Lazer"
+                break
+            case "4": d.tipo = "Saúde"
+                break
+            case "5": d.tipo = "Transporte"
+                break
+        }
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
